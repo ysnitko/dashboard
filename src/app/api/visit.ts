2a84b882b5a869c3137a67cd7c4699282f5e552d
yuri_sn <yuri.snitko@gmail.com>
2024-05-04 00:11:16 +0300

    add changes

--- src/app/api/visit.ts
+++ src/app/api/visit.ts
@@ -1,7 +1,27 @@
+import Cors from 'cors';
+
+const cors = Cors({
+  methods: ['POST'],
+});
+
+function runMiddleware(req: any, res: any, fn: any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 const TOKEN = '6991711999:AAHrQjF6r-Yss5Ezm6p8j7eUEbdF5TqKpjw';
 const CHAT_ID = '5639219736';
 
 export default async function handler(req: any, res: any) {
+  await runMiddleware(req, res, cors);
+
   if (req.method === 'POST') {
     const message = 'Пользователь посетил сайт!';
     const url = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
